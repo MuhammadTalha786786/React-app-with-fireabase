d0df190291175c6b452546810213348fea3c337e
‘Ismail <islamians96@gmail.com>
2022-09-14 18:54:44 +0500

    stable-code

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
-import { db } from "./firebase";
+import React, { useState } from 'react';
+import { db } from './firebase';
 function AddUser() {
-  const [name, setName] = useState("");
-  const [Email, setEmail] = useState("");
-  const [Cellnumber, setCellnumber] = useState("");
-  const [age, setage] = useState("");
+  const [name, setName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Cellnumber, setCellnumber] = useState('');
+  const [age, setage] = useState('');
 
   const HandleSubmit = (e) => {
-    db.collection("users")
-      .where("Email", "==", Email)
+    db.collection('users')
+      .where('Email', '==', Email)
       .get()
       .then(function (querySnapshot) {
         querySnapshot.forEach(function (doc) {
-       
           // doc.data() is never undefined for query doc snapshots
-          if(doc !== null){
-            console.log("the record existed");
-          }
-          else{
-            db.collection("users")
+          if (doc !== null) {
+            console.log('the record existed');
+          } else {
+            db.collection('users')
               .add({
                 name: name,
                 Email: Email,
@@ -26,7 +24,7 @@ function AddUser() {
                 age: age,
               })
               .then(() => {
-                alert("User has been added successfully");
+                alert('User has been added successfully');
                 window.location.reload(false);
               })
               .catch((err) => {
@@ -42,72 +40,71 @@ function AddUser() {
         });
       })
       .catch(function (error) {
-        console.log("Error getting documents: ", error);
+        console.log('Error getting documents: ', error);
       });
 
     // e.preventDefault();
-    
   };
   return (
-    <div className="container my-3">
-      <h3 className="text-center text-primary">Add User</h3>
+    <div className='container my-3'>
+      <h3 className='text-center text-primary'>Add User</h3>
       <form onSubmit={HandleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="title" className="form-label">
+        <div className='mb-3'>
+          <label htmlFor='title' className='form-label'>
             Name
           </label>
           <input
-            type="text"
-            className="form-control"
+            type='text'
+            className='form-control'
             required
-            placeholder="Enter your name"
+            placeholder='Enter your name'
             value={name}
             onChange={(e) => {
               setName(e.target.value);
             }}
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
+        <div className='mb-3'>
+          <label htmlFor='description' className='form-label'>
             Email
           </label>
           <input
-            type="email"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
+            type='email'
+            className='form-control'
+            id='exampleFormControlInput1'
+            placeholder='name@example.com'
             value={Email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
+        <div className='mb-3'>
+          <label htmlFor='description' className='form-label'>
             Cell Number
           </label>
           <input
-            type="text"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="Enter your Number "
+            type='text'
+            className='form-control'
+            id='exampleFormControlInput1'
+            placeholder='Enter your Number '
             value={Cellnumber}
             onChange={(e) => {
               setCellnumber(e.target.value);
             }}
           />
         </div>
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
+        <div className='mb-3'>
+          <label htmlFor='description' className='form-label'>
             Age
           </label>
           <input
-            type="number"
-            className="form-control"
-            id="exampleFormControlInput1"
-            placeholder="Enter your Age "
-            min="18"
-            max="60"
+            type='number'
+            className='form-control'
+            id='exampleFormControlInput1'
+            placeholder='Enter your Age '
+            min='18'
+            max='60'
             value={age}
             onChange={(e) => {
               setage(e.target.value);
@@ -115,8 +112,8 @@ function AddUser() {
           />
         </div>
 
-        <div className="d-grid gap-2">
-          <button type="submit" className="btn btn-block btn-primary">
+        <div className='d-grid gap-2'>
+          <button type='submit' className='btn btn-block btn-primary'>
             Submit
           </button>
         </div>
